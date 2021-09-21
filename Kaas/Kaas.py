a8317311a3f6e8d07cd6c777e500cf5d47ce74bd
99063871 <99063871@mydavinci.com>
2021-09-21 14:02:06 +0200

    kaas gemaakt en half af

---
+++ Kaas/Kaas.py
@@ -0,0 +1,33 @@
+Yellow = input("Is de kaas geel?")
+
+if Yellow == "ja":
+    YellowHole = input("Zitten er gaten in?")
+
+    if YellowHole == "ja":
+        HoleExpensive = input("Is de kaas belachelijk duur?")
+
+        if HoleExpensive == "ja":
+            print("Emmenthaler")
+        elif HoleExpensive == "nee":
+            print("Leerdammer")
+
+    elif YellowHole == "nee":
+        NHoleHard = input("Is de kaas hard als steen?")
+
+        if NHoleHard == "ja":
+            print("Pammigiano Reggiano")
+
+        elif NHoleHard == "nee":
+            print("Goudse kaas") 
+
+elif Yellow == "nee":
+    NYellowMold = input("Heeft de kaas blauwe schimmels?")
+
+    if NYellowMold == "ja":
+        MoldCrust = input("Heeft de kaas een korst?")
+
+        if MoldCrust == "ja":
+            print("Bleu de Rochbaron")
+        
+        elif MoldCrust == "nee":
+            print("Foume d'Amber")
