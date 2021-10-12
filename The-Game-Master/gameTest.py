7af4f0bbe90f97a0aba93a772d1b627c16251344
99063871 <99063871@mydavinci.com>
2021-10-12 11:47:44 +0200

    heel het werk gedeelte gemaakt, en het begin van het begin gemaakt van de bankoverval

--- The-Game-Master/gameTest.py
+++ The-Game-Master/gameTest.py
@@ -1,7 +1,9 @@
 import time
 import random
 
-work = "a"
+work = 0
+varA = 0
+varB = 0
 
 def varthing():
     global varA
@@ -12,15 +14,13 @@ def math():
     numB =  random.randint(1, 1000)
     numAstr = str(numA)
     numBstr = str(numB)
+    anwser = numA + numB
+    print(anwser)
     question = input("Wat is " + numAstr + " + " + numBstr + "? ")
     anwserFrom = int(question)
     anwser = numA + numB
     if anwserFrom == anwser:
         varthing()
-
-work = 0
-varA = 0
-varB = 0
 
 print("Je loopt in de stad en ziet een auto die je heel erg mooi vind, het is de Fiat multipla!")
 time.sleep(3)
@@ -63,10 +63,31 @@ def workOrRobberyFunc():
             math()
             math()
         else:
-            print("Dat ging niet helemaal goed probeer het nog eens")
+            print("Dat ging niet helemaal goed we beginnen opnieuw")
             workOrRobberyFunc()
-            if varA == "3":
-                print("Dat ging heel goed je bent aangenomen!")
-                time.sleep
+        if varA == 3:
+            time.sleep(2)
+            print("Dat ging heel erg goed! je bent aangenomen je krijgt via mail te horen wanneer je begint")
+            time.sleep(2)
+            print("2 uurtjes later kreeg je te horen dat je morgen 17:30 kon beginnen")
+            time.sleep(2)
+            print("Na een paar jaar werken en sparen heb je eindelijk het geld verzameld")
+            time.sleep(2)
+            print("Je hebt nu je droom auto gekocht, maar wat nu?")
+            time.sleep(2)
+            print("Een rijbewijs...")
+        elif varA != 3:
+            print("Jammer het is niet gelukt je bent niet aangenomen.")
+            time.sleep(2)
+            print('Je zegt tegen jezelf "Dan toch maar de bank overvallen"')
+    elif workOrRobbery == "b":
+                        print("ha")
+    else:
+        print("Dat ging niet helemaal goed probeer het nog eens")
+        workOrRobberyFunc()
+
+        
+
+
 workOrRobberyFunc()
 
